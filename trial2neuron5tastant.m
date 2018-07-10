9c317b5392f7669b1dc4af1335eef11e827f35a4
kkke <llkong.ck@gmail.com>
2018-07-10 14:52:49 -0400

    convert trial to neuron organization

--- trial2neuron5tastant.m
+++ trial2neuron5tastant.m
@@ -7,6 +7,7 @@ for n = 1:size(trial(1).trace,1)
 for i = 1: length(trial)
     if ~isnan(trial(i).S)
         S_trace_dF(it,:) = trial(i).traceSmooth_dF(n,:);
+        S_licks{it}    = trial(i).licks - trial(i).S(1);
         it =1+it;
     end
 end
@@ -16,6 +17,7 @@ it = 1;
 for i = 1: length(trial)
     if ~isnan(trial(i).N)
         M_trace_dF(it,:) = trial(i).traceSmooth_dF(n,:);
+        M_licks{it}    = trial(i).licks - trial(i).N(1);
         it =1+it;
     end
 end
@@ -25,6 +27,7 @@ it = 1;
 for i = 1: length(trial)
     if ~isnan(trial(i).CA)
         CA_trace_dF(it,:) = trial(i).traceSmooth_dF(n,:);
+        CA_licks{it}    = trial(i).licks - trial(i).CA(1);
         it =1+it;
     end
 end
@@ -34,6 +37,7 @@ it = 1;
 for i = 1: length(trial)
     if ~isnan(trial(i).Q)
         Q_trace_dF(it,:) = trial(i).traceSmooth_dF(n,:);
+        Q_licks{it}    = trial(i).licks - trial(i).Q(1);
         it =1+it;
     end
 end
@@ -42,6 +46,7 @@ it = 1;
 for i = 1: length(trial)
     if ~isnan(trial(i).W)
         W_trace_dF(it,:) = trial(i).traceSmooth_dF(n,:);
+        W_licks{it}    = trial(i).licks - trial(i).W(1);
         it =1+it;
     end
 end
@@ -109,6 +114,17 @@ neuron(n).M_Taste_dF = M_Taste_dF;
 neuron(n).CA_Taste_dF = CA_Taste_dF;
 neuron(n).Q_Taste_dF = Q_Taste_dF;
 neuron(n).W_Taste_dF = W_Taste_dF;
+
+neuron(n).S_licks    = S_licks;
+neuron(n).M_licks    = M_licks;
+neuron(n).CA_licks    = CA_licks;
+neuron(n).Q_licks    = Q_licks;
+neuron(n).W_licks    = W_licks;
+
+
+
+
+
 
 end
 %%
