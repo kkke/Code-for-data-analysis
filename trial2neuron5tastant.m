21d1af0115f73e410f60fc777ea947e6d89942ba
kkke <llkong.ck@gmail.com>
2018-07-10 14:11:53 -0400

    convert trial structure to neuron organization
    For having 5 tastants and part of the Imaging_analysis_GC3

---
+++ trial2neuron5tastant.m
@@ -0,0 +1,118 @@
+%% this code is to reorganize the trial structure into neuron structure
+
+function neuron = trial2neuron5tastant(trial);
+for n = 1:size(trial(1).trace,1)
+    
+ it = 1;
+for i = 1: length(trial)
+    if ~isnan(trial(i).S)
+        S_trace_dF(it,:) = trial(i).traceSmooth_dF(n,:);
+        it =1+it;
+    end
+end
+
+% n =12;
+it = 1;
+for i = 1: length(trial)
+    if ~isnan(trial(i).N)
+        M_trace_dF(it,:) = trial(i).traceSmooth_dF(n,:);
+        it =1+it;
+    end
+end
+
+% n =12;
+it = 1;
+for i = 1: length(trial)
+    if ~isnan(trial(i).CA)
+        CA_trace_dF(it,:) = trial(i).traceSmooth_dF(n,:);
+        it =1+it;
+    end
+end
+
+% n =12;
+it = 1;
+for i = 1: length(trial)
+    if ~isnan(trial(i).Q)
+        Q_trace_dF(it,:) = trial(i).traceSmooth_dF(n,:);
+        it =1+it;
+    end
+end
+
+it = 1;
+for i = 1: length(trial)
+    if ~isnan(trial(i).W)
+        W_trace_dF(it,:) = trial(i).traceSmooth_dF(n,:);
+        it =1+it;
+    end
+end
+
+it = 1;
+for i = 1: length(trial)
+    if ~isnan(trial(i).S)
+        S_Taste_dF(it,:) = trial(i).Taste(n,:);
+        it =1+it;
+    end
+end
+
+% n =12;
+it = 1;
+for i = 1: length(trial)
+    if ~isnan(trial(i).N)
+        M_Taste_dF(it,:) = trial(i).Taste(n,:);
+        it =1+it;
+    end
+end
+
+% n =12;
+it = 1;
+for i = 1: length(trial)
+    if ~isnan(trial(i).CA)
+        CA_Taste_dF(it,:) = trial(i).Taste(n,:);
+        it =1+it;
+    end
+end
+
+% n =12;
+it = 1;
+for i = 1: length(trial)
+    if ~isnan(trial(i).Q)
+        Q_Taste_dF(it,:) = trial(i).Taste(n,:);
+        it =1+it;
+    end
+end
+
+it = 1;
+for i = 1: length(trial)
+    if ~isnan(trial(i).W)
+        W_Taste_dF(it,:) = trial(i).Taste(n,:);
+        it =1+it;
+    end
+end
+
+for i = 1: length(trial)
+    trace_dF(i,:) = trial(i).traceSmooth_dF(n,:);
+end
+
+neuron(n).trial = trial;
+neuron(n).framT = trial(1).framT;
+neuron(n).T     = trial(1).Tpro;
+neuron(n).trace_dF   = trace_dF;
+neuron(n).S_trace_dF = S_trace_dF;
+neuron(n).M_trace_dF = M_trace_dF;
+neuron(n).CA_trace_dF = CA_trace_dF;
+neuron(n).Q_trace_dF = Q_trace_dF;
+neuron(n).W_trace_dF = W_trace_dF;
+
+
+neuron(n).S_Taste_dF = S_Taste_dF;
+neuron(n).M_Taste_dF = M_Taste_dF;
+neuron(n).CA_Taste_dF = CA_Taste_dF;
+neuron(n).Q_Taste_dF = Q_Taste_dF;
+neuron(n).W_Taste_dF = W_Taste_dF;
+
+end
+%%
+
+%
+%% statistical test
+
