e4ed7b4aa54054ff4d4e45c770f3731ca6936b7f
kkke <llkong.ck@gmail.com>
2018-08-02 19:36:57 -0400

    bar plot for matrix data

--- barplot_equal.m
+++ barplot_equal.m
@@ -9,11 +9,12 @@ sem2 = std(control(:,2))./sqrt(length(control(:,3)));
 sem3 = std(control(:,3))./sqrt(length(control(:,3)));
 
 figure;
-bar(1,m1,'EdgeColor',[0 0 0],'FaceColor','r','LineWidth',1.5);hold on
-bar(2,m2,'EdgeColor',[0 0 1],'FaceColor','g','LineWidth',1.5);hold on
-bar(3,m3,'EdgeColor',[0 0 1],'FaceColor','g','LineWidth',1.5);hold on
+bar(1,m1,'EdgeColor',[0 0 0],'FaceColor',[0.5,0.5,1],'LineWidth',1);hold on
+bar(2,m2,'EdgeColor',[0 0 0],'FaceColor',[1,0.5,0.25],'LineWidth',1);hold on
+bar(3,m3,'EdgeColor',[0 0 0],'FaceColor',[1,0.5,0.25],'LineWidth',1);hold on
 set(gca,'XTick',0:4)
 set(gca,'xticklabel',name)
+ylabel('Licking Rate (Hz)')
 hold on;
 e=errorbar([m1, m2, m3], [sem1, sem2, sem3],'LineWidth',1.5);
 % plot([1 1],[mean(a_m) mean(a_m)+a_sem],'r');hold on;
