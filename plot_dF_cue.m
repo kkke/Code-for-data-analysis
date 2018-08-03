fa409c5a5583f40d96388203d41b1d52c35b6c5c
kkke <llkong.ck@gmail.com>
2018-08-03 15:29:00 -0400

    plot dF/F for cue response

---
+++ plot_dF_cue.m
@@ -0,0 +1,72 @@
+function plot_dF(i,neuron)
+% i = 25;
+figure
+for j = 1:size(neuron(i).trace_dF,1)
+    plot(neuron(i).framT,neuron(i).trace_dF(j,:),'Color',[0.5,0.5,0.5,0.5])
+    hold on
+end
+plot(neuron(i).framT,mean(neuron(i).trace_dF,1),'r')
+xlim([-1,5])
+xlabel('Time (s)')
+ylabel('dF/F')
+title('Tone response')
+
+% figure
+% for j = 1:size(neuron(i).M_Taste_dF,1)
+%     plot(neuron(i).T,neuron(i).M_Taste_dF(j,:),'Color',[0.5,0.5,0.5,0.5])
+%     hold on
+% end
+% plot(neuron(i).T,mean(neuron(i).M_Taste_dF,1))
+% xlim([-2,4])
+% xlabel('Time (s)')
+% ylabel('dF/F')
+% title('Maltose')
+
+% figure
+% for j = 1:size(neuron(i).CA_Taste_dF,1)
+%     plot(neuron(i).T,neuron(i).CA_Taste_dF(j,:),'Color',[0.5,0.5,0.5,0.5])
+%     hold on
+% end
+% plot(neuron(i).T,mean(neuron(i).CA_Taste_dF,1))
+% xlim([-2,4])
+% xlabel('Time (s)')
+% ylabel('dF/F')
+% title('Quinine')
+
+% figure
+% for j = 1:size(neuron(i).Q_Taste_dF,1)
+%     plot(neuron(i).T,neuron(i).Q_Taste_dF(j,:),'Color',[0.5,0.5,0.5,0.5])
+%     hold on
+% end
+% plot(neuron(i).T,mean(neuron(i).Q_Taste_dF,1))
+% xlim([-2,4])
+% xlabel('Time (s)')
+% ylabel('dF/F')
+% title('Cycloheximide')
+
+% figure
+% for j = 1:size(neuron(i).W_Taste_dF,1)
+%     plot(neuron(i).T,neuron(i).W_Taste_dF(j,:),'Color',[0.5,0.5,0.5,0.5])
+%     hold on
+% end
+% plot(neuron(i).T,mean(neuron(i).W_Taste_dF,1))
+% xlim([-2,4])
+% xlabel('Time (s)')
+% ylabel('dF/F')
+% title('Water')
+% 
+% figure
+% plot(neuron(i).T,mean(neuron(i).S_Taste_dF,1))
+% hold on
+% plot(neuron(i).T,mean(neuron(i).M_Taste_dF,1))
+% % plot(neuron(i).T,mean(neuron(i).CA_Taste_dF,1))
+% % plot(neuron(i).T,mean(neuron(i).Q_Taste_dF,1))
+% plot(neuron(i).T,mean(neuron(i).W_Taste_dF,1))
+% % legend('S','M','Q','Cyx','W')
+% legend('S','M','W')
+% xlim([-2,4])
+% xlabel('Time (s)')
+% ylabel('dF/F')
+% title('Taste response')
+
+
