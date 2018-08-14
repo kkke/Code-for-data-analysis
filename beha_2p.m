aa328536556bd0c59d830894cae351d4bf38c5b9
kkke <llkong.ck@gmail.com>
2018-08-14 10:56:13 -0400

    behavioral analysis for 5 tastes (2p Intan)

--- beha_2p.m
+++ beha_2p.m
@@ -1,11 +1,12 @@
 %% behaviral analysis for 2p rig
 %% Step 1: load data from Intan
+clear
 file = dir('*.rhd');
 filename = file.name
 data = read_Intan(filename);
-[Frame,~] = Timing_onset_offset(data.event(7,:), data.ts, 0.5,10,1);
-[Tone,~]  = Timing_onset_offset(data.event(6,:), data.ts, 0.5,10,1);
-[W,~]     = Timing_onset_offset(data.event(5,:), data.ts, 0.5,10,1);
+[Frame,~] = Timing_onset_offset(data.event(7,:), data.ts, 0.5,10,0);
+[Tone,~]  = Timing_onset_offset(data.event(6,:), data.ts, 0.5,10,0);
+[W,~]     = Timing_onset_offset(data.event(5,:), data.ts, 0.5,10,0);
 [Q,~]     = Timing_onset_offset(data.event(4,:), data.ts, 0.5,10,0);
 [CA,~]    = Timing_onset_offset(data.event(3,:), data.ts, 0.5,10,0);
 [N,~]     = Timing_onset_offset(data.event(2,:), data.ts, 0.5,10,0);
@@ -96,6 +97,7 @@ for i = 1:length(summarydata.S_ps)
     id = find(summarydata.Tone<summarydata.S_ps(i) & summarydata.Tone>summarydata.S_ps(i)-5);
     if ~isempty(id)
         h1 = scatter(Tone_lick.spikeraster(id).times,i*ones(size(Tone_lick.spikeraster(id).times)),6,'cv','filled');
+        iti.s{i} = diff(Tone_lick.spikeraster(id).times);
     end   
     hold on
 end
@@ -104,6 +106,7 @@ for j = 1:length(summarydata.N_ps)
     i = i+1;
     if ~isempty(id)
         h2 = scatter(Tone_lick.spikeraster(id).times,i*ones(size(Tone_lick.spikeraster(id).times)),6,'mv','filled');
+        iti.n{j} = diff(Tone_lick.spikeraster(id).times);
     end   
 end
 for j = 1:length(summarydata.CA_ps)
@@ -111,6 +114,7 @@ for j = 1:length(summarydata.CA_ps)
     i = i+1;
     if ~isempty(id)
         h3 = scatter(Tone_lick.spikeraster(id).times,i*ones(size(Tone_lick.spikeraster(id).times)),6,'bv','filled');
+        iti.c{j} = diff(Tone_lick.spikeraster(id).times);
     end   
 end
 for j = 1:length(summarydata.Q_ps)
@@ -118,6 +122,7 @@ for j = 1:length(summarydata.Q_ps)
     i = i+1;
     if ~isempty(id)
         h4 = scatter(Tone_lick.spikeraster(id).times,i*ones(size(Tone_lick.spikeraster(id).times)),6,'rv','filled');
+        iti.q{j} = diff(Tone_lick.spikeraster(id).times);
     end   
 end
 for j = 1:length(summarydata.W_ps)
@@ -125,13 +130,121 @@ for j = 1:length(summarydata.W_ps)
     i = i+1;
     if ~isempty(id)
         h5 = scatter(Tone_lick.spikeraster(id).times,i*ones(size(Tone_lick.spikeraster(id).times)),6,'kv','filled');
+        iti.w{j} = diff(Tone_lick.spikeraster(id).times);
     end   
 end
-legend([h1,h2,h3,h4,h5],{'S','M','Q','C','W'})
+legend([h1,h2,h3,h4,h5],{'S','N','C','Q','W'})
 rectangle('Position',[0,0,2,i],'FaceColor',[0.5 .5 .5,0.5],'EdgeColor',[0.5 .5 .5,0])
 xlim([-2,8])
 xlabel('Time (s)')
 ylabel('Trial #')
 set(gca,'TickDir','out');
 %%
-save('summary.mat','summarydata')
+iti_s = cell2mat(iti.s);
+iti_n = cell2mat(iti.n);
+iti_c = cell2mat(iti.c);
+iti_q = cell2mat(iti.q);
+iti_w = cell2mat(iti.w);
+
+figure;
+[h1,x1] = ecdf(iti_s(find(iti_s<0.5)));
+hold on
+[h2,x2] = ecdf(iti_n(find(iti_n<0.5)));
+[h3,x3] = ecdf(iti_c(find(iti_c<0.5)));
+[h4,x4]= ecdf(iti_q(find(iti_q<0.5)));
+[h5,x5]= ecdf(iti_w(find(iti_w<0.5)));
+
+plot(x1,h1,'c')
+hold on
+plot(x2,h2,'m')
+plot(x3,h3,'b')
+plot(x4,h4,'r')
+plot(x5,h5,'k')
+xlabel('Inter-lick interval (s)')
+ylabel('Cumulative proportion')
+%%
+t =1;
+figure
+lick_S = spike2eventRasteandPSTH_NP (summarydata.licks,summarydata.S_ps, 100, -3000, 5000);
+for j = 1:length(summarydata.S_ps)
+    if ~isempty(lick_S.spikeraster(j).times)
+        h1 = scatter(lick_S.spikeraster(j).times,j*ones(size(lick_S.spikeraster(j).times)),6,'cv','filled');
+        idx = find(lick_S.spikeraster(j).times>0 & lick_S.spikeraster(j).times<=t);       
+        lickrate.s{j} = diff(lick_S.spikeraster(j).times(idx));
+        hold on
+    end
+end
+i = j;
+lick_N = spike2eventRasteandPSTH_NP (summarydata.licks,summarydata.N_ps, 100, -3000, 5000);
+for j = 1:length(summarydata.N_ps)
+    i = i+1;
+    if ~isempty(lick_N.spikeraster(j).times)
+        h1 = scatter(lick_N.spikeraster(j).times,i*ones(size(lick_N.spikeraster(j).times)),6,'mv','filled');
+        idx = find(lick_N.spikeraster(j).times>0 & lick_N.spikeraster(j).times<=t);
+        lickrate.n{j} = diff(lick_N.spikeraster(j).times(idx));
+        hold on
+    end
+end
+
+lick_C = spike2eventRasteandPSTH_NP (summarydata.licks,summarydata.CA_ps, 100, -3000, 5000);
+for j = 1:length(summarydata.CA_ps)
+    i = i+1;
+    if ~isempty(lick_C.spikeraster(j).times)
+        h1 = scatter(lick_C.spikeraster(j).times,i*ones(size(lick_C.spikeraster(j).times)),6,'bv','filled');
+        idx = find(lick_C.spikeraster(j).times>0 & lick_C.spikeraster(j).times<=t);
+        lickrate.c{j} = diff(lick_C.spikeraster(j).times(idx));
+        hold on
+    end
+end
+
+lick_Q = spike2eventRasteandPSTH_NP (summarydata.licks,summarydata.Q_ps, 100, -3000, 5000);
+for j = 1:length(summarydata.Q_ps)
+    i = i+1;
+    if ~isempty(lick_Q.spikeraster(j).times)
+        h1 = scatter(lick_Q.spikeraster(j).times,i*ones(size(lick_Q.spikeraster(j).times)),6,'rv','filled');
+        idx = find(lick_Q.spikeraster(j).times>0 & lick_Q.spikeraster(j).times<=t);
+        lickrate.q{j} = diff(lick_Q.spikeraster(j).times(idx));
+        hold on
+    end
+end
+
+lick_W = spike2eventRasteandPSTH_NP (summarydata.licks,summarydata.W_ps, 100, -3000, 5000);
+for j = 1:length(summarydata.W_ps)
+    i = i+1;
+    if ~isempty(lick_W.spikeraster(j).times)
+        h1 = scatter(lick_W.spikeraster(j).times,i*ones(size(lick_W.spikeraster(j).times)),6,'kv','filled');
+        idx = find(lick_W.spikeraster(j).times>0 & lick_W.spikeraster(j).times<=t);
+        lickrate.w{j} = diff(lick_W.spikeraster(j).times(idx));
+        hold on
+    end
+end
+
+xlabel('Time (s)')
+ylabel('Trial #')
+xlim([-3,5])
+%%
+lickrate_s = cell2mat(lickrate.s);
+lickrate_n = cell2mat(lickrate.n);
+lickrate_c = cell2mat(lickrate.c);
+lickrate_q = cell2mat(lickrate.q);
+lickrate_w = cell2mat(lickrate.w);
+
+lickrate_avg = 1/mean([lickrate_s,lickrate_n,lickrate_c,lickrate_q]);
+%%
+% figure;
+% [h1,x1] = ecdf(lickrate_s);
+% hold on
+% [h2,x2] = ecdf(lickrate_n);
+% [h3,x3] = ecdf(lickrate_c);
+% [h4,x4]= ecdf(lickrate_q);
+% [h5,x5]= ecdf(lickrate_w);
+% 
+% plot(x1,h1,'c')
+% hold on
+% plot(x2,h2,'m')
+% plot(x3,h3,'b')
+% plot(x4,h4,'r')
+% plot(x5,h5,'k')
+
+%%
+save('summary.mat','summarydata','lickrate_avg')
