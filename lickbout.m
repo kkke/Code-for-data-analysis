7d74518923ab701c5c247beae830e46eb07dc177
kkke <llkong.ck@gmail.com>
2018-10-01 17:09:12 -0400

    convert licking event to licking bout

---
+++ lickbout.m
@@ -0,0 +1,27 @@
+function cen_duration = lickbout(cen_L)
+ili=diff(cen_L);
+b=find(ili>0.5);     % inter lick interval is bigger than 500 ms, which means a new licking bouts
+for i=1:length(b)
+    if i==1;
+        bouts{1}=1:b(1);
+    else
+        bouts{i}=b(i-1)+1:b(i);
+    end
+end
+bouts{i+1}=b(i)+1:length(cen_L); % all the bouts including less than 3 licks
+lickSpont=[];
+% find out the bouts which has less than 3 licks
+for i=1:length(bouts)
+    if length(bouts{i})<3
+       randlick=bouts{i};
+       bouts{i}=[];
+       lickSpont=[lickSpont randlick];
+    end  
+end
+bouts=bouts(~cellfun('isempty',bouts)); % all the real bouts 
+for i = 1:length(bouts)
+    lickbout{i} = cen_L(bouts{i});
+    cen_duration(i) = lickbout{i}(end) - lickbout{i}(1);
+end
+figure;
+histogram(cen_duration,20)
